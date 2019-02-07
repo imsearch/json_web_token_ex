27ccd891cb28f3ee26dbdf8ae73d57e20841692c
Bryan Tylor <bryan.tylor@gmail.com>
2019-02-07 10:47:48 -0500

    prefix with extra_

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule JsonWebToken.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [
-      applications: [
+      extra_applications: [
         :crypto,
         :logger,
         :public_key,
